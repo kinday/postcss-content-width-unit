46c3b3bd000e68132aa328c7d0feffbbc4baa0b3
Leonard Kinday <leonard@kinday.ru>
2016-11-21 12:48:48 +0100

    Add endsWith test

---
+++ endsWith.test.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+
+import endsWith from './endsWith';
+
+test('endsWith', t => {
+    t.truthy(
+        endsWith('rpx', '200rpx'),
+        '$content-max-width / 200',
+        'is true if string ends with given pattern'
+    );
+
+    t.falsy(
+        endsWith('rpx', '200px'),
+        'is false if string doesn’t end with given pattern'
+    );
+});
